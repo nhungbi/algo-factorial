011164b2609831e13722f3166f760bd19a89db5c
nhungbi <nhungvan16@gmail.com>
2022-05-24 10:43:28 -0500

    fn both

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,8 @@
 exports.factorial = function(num) {
+    if (num <= 1) {
+        return 1
+    }
+
+    return num*exports.factorial(num-1)
+
 };
--- python/factorial.py
+++ python/factorial.py
@@ -1,2 +1,5 @@
 def factorial(num):
-	# your code here
+	if num <= 0:
+		return 1
+	
+	return num*factorial(num-1)
